2167c6ccb0656e09efb0be538715047965235e68
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-02-25 12:12:01 +0530

    migrtions

--- app1/laravel/database/migrations/2023_02_25_053637_create-admin.php
+++ app1/laravel/database/migrations/2023_02_25_053637_create-admin.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateAdmin extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('admin', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('email',255);
+            $table->string('password',255);
+            $table->timestamp('created_at');
         });
     }
 
@@ -27,6 +29,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('admin', function (Blueprint $table) {
+            //
+        });
     }
 }
